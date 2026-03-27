90379f403cbc04f8459cc777e9e7d70b99a32b8e
Manu <manmdyalw@gmail.com>
2026-03-27 12:07:22 +0100

    first parameterized test works

--- src/test/java/be/technifutur/M/RequestTranslatorTest.java
+++ src/test/java/be/technifutur/M/RequestTranslatorTest.java
@@ -1,13 +1,23 @@
 package be.technifutur.M;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class RequestTranslatorTest {
     @Test
     void testSelectingTheAddParticipantSucceeds() {
         RequestTranslator req = new RequestTranslator();
         var result = req.translate("1");
-        Assertions.assertEquals(1,result);
+        Assertions.assertEquals(1, result);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void testSelectingTheTheThreeOptionsSucceeds(int choice) {
+        RequestTranslator req = new RequestTranslator();
+        var result = req.translate(String.valueOf(choice));
+        Assertions.assertEquals(choice, result);
     }
 }

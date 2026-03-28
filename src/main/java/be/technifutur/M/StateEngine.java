c7fdd351e276e58b1e13f7ca7434405e64e1e29a
Manu <manmdyalw@gmail.com>
2026-03-28 10:20:50 +0100

    a comprised option lead to expected navigation

--- src/main/java/be/technifutur/M/StateEngine.java
+++ src/main/java/be/technifutur/M/StateEngine.java
@@ -13,8 +13,11 @@ public class StateEngine {
     }
 
     public void apply(int i) {
-        if(i == 1){
-            this.currentState = States.PLAGE_MANAGEMENT.getValue();
-        }
+        this.currentState = switch(i){
+            case 1 -> States.PLAGE_MANAGEMENT.getValue();
+            case 2 -> States.PEOPLE_MANAGEMENT.getValue();
+            case 3 -> States.ADMIN_MANAGEMENT.getValue();
+            default -> throw new IllegalStateException("Unexpected value: " + i);
+        };
     }
 }
--- src/test/java/be/technifutur/M/StateEngineTest.java
+++ src/test/java/be/technifutur/M/StateEngineTest.java
@@ -1,6 +1,11 @@
 package be.technifutur.M;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -14,11 +19,27 @@ class StateEngineTest {
     }
 
     @Test
-    public void aCompriseActionLeadToAttendedNavigation(){
+    public void aCompriseActionLeadsToAttendedNavigation(){
         StateEngine engine = new StateEngine(States.MAIN_MENU);
         engine.apply(1);
 
         assertEquals("b",engine.getCurrentState());
     }
 
+    static List<Arguments> actionsForNavigation(){
+        return List.of(
+                Arguments.of(1,States.PLAGE_MANAGEMENT),
+                Arguments.of(2,States.PEOPLE_MANAGEMENT),
+                Arguments.of(3,States.ADMIN_MANAGEMENT)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("actionsForNavigation")
+    public void compriseActionOnMainMenuLeadToAttendedNavigation(int entry, States state){
+        StateEngine engine = new StateEngine(States.MAIN_MENU);
+        engine.apply(entry);
+
+        assertEquals(state.getValue(),engine.getCurrentState());
+    }
 }
